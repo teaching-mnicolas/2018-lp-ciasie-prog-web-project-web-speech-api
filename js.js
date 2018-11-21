5c5adf797a523897aed1a1c3767b0bd066521be6
= <=>
2018-11-21 10:17:00 +0100

    Modif style et div interview

--- js.js
+++ js.js
@@ -1,4 +1,18 @@
 $('.switch').click(function(){
+
+  if ($('.selected').find('input').val().length > 1){
+
+    $('.interview').append('<p><span class="name">' + $('.selected').find('input').val()  + '</span></p>')
     $('#1').toggleClass('selected');
     $('#2').toggleClass('selected');
-  })
+
+  }
+
+});
+  
+
+  if (!('webkitSpeechRecognition' in window)) {
+    upgrade();
+  } else {
+    console.log("test");
+  }
