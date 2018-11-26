2ee999467a0b69e2b62c76ad7e2d601338de26bf
Briand-Lucas <lucas.briand88@outlook.fr>
2018-11-26 11:09:00 +0100

    ADD méthodes liées à l'API web speech

--- js.js
+++ js.js
@@ -7,12 +7,65 @@ $('.switch').click(function(){
     $('#2').toggleClass('selected');
 
   }
+  var final_transcript = '';
+  var recognizing = false;
 
-});
-  
+  if ('webkitSpeechRecognition' in window) {
 
-  if (!('webkitSpeechRecognition' in window)) {
-    upgrade();
-  } else {
-    console.log("test");
+    var recognition = new webkitSpeechRecognition();
+
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onstart = function() {
+      recognizing = true;
+    };
+
+    recognition.onerror = function(event) {
+      console.log(event.error);
+    };
+
+    recognition.onend = function() {
+      recognizing = false;
+    };
+
+    recognition.onresult = function(event) {
+      var interim_transcript = '';
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
+      }
+      final_transcript = capitalize(final_transcript);
+      final_span.innerHTML = linebreak(final_transcript);
+      interim_span.innerHTML = linebreak(interim_transcript);
+      console.log(final_transcript);
+      
+    };
   }
+
+  var two_line = /\n\n/g;
+  var one_line = /\n/g;
+  function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+  }
+
+  function capitalize(s) {
+    return s.replace(s.substr(0,1), function(m) { return m.toUpperCase(); });
+  }
+
+  function startDictation(event) {
+    if (recognizing) {
+      recognition.stop();
+      return;
+    }
+    final_transcript = '';
+    recognition.lang = 'fr-FR';
+    recognition.start();
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+  }
+
+  });
